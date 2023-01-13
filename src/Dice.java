c72ac5d6688d49cacac8c0af2bba5a9ddb5a8e64
roboanonymous <jsamyak591@gmail.com>
2023-01-13 12:29:14 +0530

    first

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Dice {
 
 	private int dice;
@@ -11,4 +11,19 @@ public class Dice {
 		this.dice = dice;
 	}
 	
+	Random r = new Random();
+	int count = 0;
+	public int rolldice(int dice)
+	{
+		System.out.print("Dice Value is: ");
+		for(int i=0; i<dice ; i++)
+		{
+			int num = r.nextInt(6)+1 ; 
+			System.out.print(num + " ");
+			count += num;
+		}
+		
+		return count;
+		
+	}
 }
